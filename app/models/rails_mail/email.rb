2778eccefa08d94b73b3941123cd1dc056e45a97
Peter Philips <pete@p373.net>
2025-01-19 15:20:22 -0800

    fix indentation

--- app/models/rails_mail/email.rb
+++ app/models/rails_mail/email.rb
@@ -1,9 +1,9 @@
 module RailsMail
   class Email < ApplicationRecord
-  store_accessor :data, :from, :to, :cc, :bcc, :subject, :body, :content_type, :attachments
+    store_accessor :data, :from, :to, :cc, :bcc, :subject, :body, :content_type, :attachments
 
-  validates :from, presence: true
-  validates :to, presence: true
-  validates :subject, presence: true
+    validates :from, presence: true
+    validates :to, presence: true
+    validates :subject, presence: true
   end
 end
