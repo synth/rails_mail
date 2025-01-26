eaef6fac0e3b5cd43804b5cac9f1ce8b94e8d290
Peter Philips <pete@p373.net>
2025-01-26 11:50:24 -0800

    Implement ability to show/hide clear button

--- lib/generators/rails_mail/install/templates/initializer.rb
+++ lib/generators/rails_mail/install/templates/initializer.rb
@@ -9,4 +9,11 @@ RailsMail.configure do |config|
     # msg = Rails.env.development? ? 'Forbidden - make sure you have the correct permission in config/initializers/rails_mail.rb' : 'Not Found'
     # raise ActionController::RoutingError.new(msg) unless user_session&.user&.admin?
   # end
+
+  config.show_clear_button do
+    # show clear button in development
+    # and prefer to trim in non-development environments
+    # to prevent accidental deletion of emails
+    Rails.env.development?
+  end
 end
