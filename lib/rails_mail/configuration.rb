8fcdaa84f68816704ed7307a11ab2c1039d6eaca
Peter Philips <pete@p373.net>
2025-01-27 21:25:14 -0800

    refactor sync_via to trim_via

--- lib/rails_mail/configuration.rb
+++ lib/rails_mail/configuration.rb
@@ -1,21 +1,21 @@
 module RailsMail
   class Configuration
     attr_accessor :authentication_callback, :show_clear_button,
-                  :trim_emails_older_than, :trim_emails_max_count, :sync_via
+                  :trim_emails_older_than, :trim_emails_max_count, :trim_via
 
     def initialize
       @authentication_callback = nil
       @show_clear_button = nil
       @trim_emails_older_than = nil
       @trim_emails_max_count = nil
-      @sync_via = :later
+      @trim_via = :perform_later
     end
 
-    def sync_via=(value)
+    def trim_via=(value)
       unless [ :now, :later ].include?(value)
-        raise ArgumentError, "sync_via must be :now or :later"
+        raise ArgumentError, "trim_via must be :now or :later"
       end
-      @sync_via = value
+      @trim_via = value
     end
 
     def authentication_callback=(callback)
