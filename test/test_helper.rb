690839d56705dc88fcb30c5e753dd51844c5caa9
Peter Philips <pete@p373.net>
2025-02-09 12:29:11 -0800

    Bring back email destroy_all and refactor to avoid helper namespace collisions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,10 @@ ActiveRecord::Migrator.migrations_paths = [ File.expand_path("../test/dummy/db/m
 require "rails/test_help"
 require "minitest/mock"
 require "debug"
+require_relative "../app/helpers/rails_mail/turbo_helper"
+
+# Register Turbo Stream MIME type
+Mime::Type.register "text/vnd.turbo-stream.html", :turbo_stream
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
