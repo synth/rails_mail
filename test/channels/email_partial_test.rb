f271a62cdeb160bffc13890b971b782bb7c94a39
Zach Grande <zach.grande23@gmail.com>
2025-02-21 12:56:21 -0800

    spec: add test for email partial broadcasting

---
+++ test/channels/email_partial_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+module RailsMail
+  class EmailPartialTest < ActionCable::Channel::TestCase
+    include Engine.routes.url_helpers
+
+    setup do
+      @email = rails_mail_emails(:one)
+      @email.send(:broadcast_email)
+    end
+
+    test "broadcasts the email partial" do
+      broadcasted_message = broadcasts("rails_mail:emails").last
+
+      assert_not_nil broadcasted_message, "Expected a broadcasted message but got nil"
+      assert_includes broadcasted_message, @email.subject
+      assert_includes broadcasted_message, @email.from
+      assert_includes broadcasted_message, email_path(@email)
+    end
+  end
+end
