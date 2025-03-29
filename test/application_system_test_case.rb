3151a799626df0e015d411e86c092873d2a6b0c7
Zach Grande <zach.grande23@gmail.com>
2025-03-29 13:50:16 -0700

    fix(spec): set data dir in selenium setup

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,10 @@ require "test_helper"
 require "capybara/rails"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ], options: {
+    options: Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      user_data_dir = File.join(Dir.tmpdir, "selenium", "user_data_#{SecureRandom.hex(8)}")
+      opts.add_argument("--user-data-dir=#{user_data_dir}")
+    end
+  }
 end
