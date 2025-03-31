4ad2727c086ac31199e1cae66525e863241c038e
Zach Grande <zach.grande23@gmail.com>
2025-03-31 14:22:20 -0700

    refactor: email tab logic with tailwind

--- app/frontend/rails_mail/application.js
+++ app/frontend/rails_mail/application.js
@@ -4,15 +4,13 @@ import EmailHighlightController from "email_highlight_controller";
 import RedirectController from "redirect_controller";
 import AutoSubmit from 'auto_submit'
 import Timeago from 'timeago'
-import EmailTabsController from 'email_tabs_controller'
 
 const application = Application.start();
 
 application.register("email-highlight", EmailHighlightController);
 application.register("redirect", RedirectController);
-application.register('auto-submit', AutoSubmit)
-application.register('timeago', Timeago)
-application.register('email-tabs', EmailTabsController);
+application.register('auto-submit', AutoSubmit);
+application.register('timeago', Timeago);
 
 window.Stimulus = Application.start();
 
