48e96e25fb25d59f3274ea633218e919429ea666
Peter Philips <pete@p373.net>
2025-04-22 14:53:23 -0700

    Fix rubocop

--- app/helpers/rails_mail/emails_helper.rb
+++ app/helpers/rails_mail/emails_helper.rb
@@ -2,13 +2,13 @@ module RailsMail
   module EmailsHelper
     def prepare_email_html(raw_html)
       doc = Nokogiri::HTML::DocumentFragment.parse(raw_html)
-      doc.css('a[href]').each do |a|
-        a.set_attribute('target', '_blank')
-        a.set_attribute('data-turbo', 'false')
+      doc.css("a[href]").each do |a|
+        a.set_attribute("target", "_blank")
+        a.set_attribute("data-turbo", "false")
       end
       sanitize(doc.to_html,
-        tags: ActionView::Base.sanitized_allowed_tags + ['table', 'tbody', 'tr', 'td'],
-        attributes: ActionView::Base.sanitized_allowed_attributes + ['style', 'target', 'data-turbo'])
+        tags: ActionView::Base.sanitized_allowed_tags + [ "table", "tbody", "tr", "td" ],
+        attributes: ActionView::Base.sanitized_allowed_attributes + [ "style", "target", "data-turbo" ])
     end
   end
 end
