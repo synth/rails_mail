49d9544803ecab8363abf12a358aae68f2fc50c9
Peter Philips <pete@p373.net>
2025-04-22 14:49:42 -0700

    Make links in email pop out and not use turbo

--- app/helpers/rails_mail/emails_helper.rb
+++ app/helpers/rails_mail/emails_helper.rb
@@ -1,4 +1,14 @@
 module RailsMail
   module EmailsHelper
+    def prepare_email_html(raw_html)
+      doc = Nokogiri::HTML::DocumentFragment.parse(raw_html)
+      doc.css('a[href]').each do |a|
+        a.set_attribute('target', '_blank')
+        a.set_attribute('data-turbo', 'false')
+      end
+      sanitize(doc.to_html,
+        tags: ActionView::Base.sanitized_allowed_tags + ['table', 'tbody', 'tr', 'td'],
+        attributes: ActionView::Base.sanitized_allowed_attributes + ['style', 'target', 'data-turbo'])
+    end
   end
 end
